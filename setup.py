e83811307c5faa182a66557dfaccf6cad5c21292
domeckert <Dominique.Eckert@unige.ch>
2022-03-02 15:25:46 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
       name='hydromass',    # This is the name of your PyPI-package.
-      version='0.1',
+      version='0.2',
       description='Hydrostatic mass calculator',
       author='Dominique Eckert, Vittorio Ghirardini, Stefano Ettori',
       author_email='Dominique.Eckert@unige.ch',
-      url="https://github.com/domeckert/pyproffit",
+      url="https://github.com/domeckert/hydromass",
       packages=['hydromass'],
       install_requires=[
-            'numpy','scipy','astropy','matplotlib','pymc3',
+            'numpy','scipy','astropy','matplotlib','pymc3','pyproffit'
       ],
       include_package_data=True,
 )
