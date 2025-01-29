fixed small issue in gas density calculation from posterior chains

build/lib/hydromass/mhyd.py
@@ -304,6 +304,10 @@ def Run_Mhyd_PyMC3(Mhyd,model,bkglim=None,nmcmc=1000,fit_bkg=False,back=None,
 
             pnt_cov = np.loadtxt(file_cov).astype(np.float32)
 
+    if Mhyd.veldata is not None and not pnt:
+
+        print('Warning: velocity data were provided but pnt is set to False. The velocity data will be ignored.')
+
     with hydro_model:
         # Model parameters
         allpmod = []
@@ -462,17 +466,25 @@ def Run_Mhyd_PyMC3(Mhyd,model,bkglim=None,nmcmc=1000,fit_bkg=False,back=None,
 
                     pth_test = press_out * (1. - alpha_turb)
 
+                    pnt_test = press_out * alpha_turb
+
                 if pnt_model == 'Ettori' :
 
                     log_pnt = beta_nt * pm.math.log(dens_m * 1e3) + logp0_nt * np.log(10)
 
                     pth_test = press_out - pm.math.exp(log_pnt)
 
+                    pnt_test = pm.math.exp(log_pnt)
+
                 pth = pm.math.switch(pth_test <= 0, 1e-10, pth_test)
+
+                pnt_prof = pm.math.switch(pnt_test <= 0, 1e-10, pnt_test)
 
             else:
 
                 pth = press_out
+
+                pnt_prof = 0
 
         if not wlonly:
             # Density Likelihood
@@ -570,6 +582,17 @@ def Run_Mhyd_PyMC3(Mhyd,model,bkglim=None,nmcmc=1000,fit_bkg=False,back=None,
 
             g_obs = pm.MvNormal('WL', mu=gmodel_elong[ev], observed=WLdata.gplus, cov=WLdata.covmat)
 
+        if Mhyd.veldata is not None and pnt:
+
+            sigmav, ev = NPmodel(Mhyd=Mhyd,
+                                 rref_m=rref_m,
+                                 dens_m=dens_m,
+                                 pnt=pnt_prof,
+                                 proj_vol=vol)
+
+            sv_obs = pm.Normal('sigmav', mu=sigmav[ev], sigma=Mhyd.veldata.vtot_error, observed=Mhyd.veldata.vtot)
+
+
     tinit = time.time()
 
     print('Running HMC...')
@@ -617,6 +640,10 @@ def Run_Mhyd_PyMC3(Mhyd,model,bkglim=None,nmcmc=1000,fit_bkg=False,back=None,
         if Mhyd.wl_data is not None:
 
             Mhyd.ppc_wl = pm.sample_posterior_predictive(trace, var_names=['WL'])
+
+        if Mhyd.veldata is not None:
+
+            Mhyd.ppc_vel = pm.sample_posterior_predictive(trace, var_names=['sigmav'])
 
 
     print('Done.')
@@ -853,7 +880,7 @@ class Mhyd:
     :type abund: str
     """
 
-    def __init__(self, sbprofile=None, spec_data=None, sz_data=None, wl_data=None, directory=None, redshift=None, cosmo=None, abund = 'aspl'):
+    def __init__(self, sbprofile=None, spec_data=None, sz_data=None, wl_data=None, vel_data=None, directory=None, redshift=None, cosmo=None, abund = 'aspl'):
 
         if abund == 'angr':
             nhc = 1 / 0.8337
@@ -933,6 +960,8 @@ class Mhyd:
         self.sz_data = sz_data
 
         self.wl_data = wl_data
+
+        self.veldata = vel_data
 
         rho_cz = cosmo.critical_density(redshift).value * cgsMpc ** 3 / Msun # critical density in Msun per Mpc^3
 

hydromass/nonparametric.py
@@ -630,11 +630,11 @@ def prof_GP_hires(Mhyd, rin=None, npt=200, Z=0.3):
 
     if Mhyd.fit_bkg:
 
-        Kdens_m = calc_density_operator(rout_m / Mhyd.amin2kpc, Mhyd.pardens, Mhyd.amin2kpc)
+        Kdens_m = calc_density_operator(rref_m / Mhyd.amin2kpc, Mhyd.pardens, Mhyd.amin2kpc)
 
     else:
 
-        Kdens_m = calc_density_operator(rout_m / Mhyd.amin2kpc, Mhyd.pardens, Mhyd.amin2kpc, withbkg=False)
+        Kdens_m = calc_density_operator(rref_m / Mhyd.amin2kpc, Mhyd.pardens, Mhyd.amin2kpc, withbkg=False)
 
     dens_m = np.sqrt(np.dot(Kdens_m, np.exp(Mhyd.samples.T)) / cf_prof * Mhyd.transf)
 

hydromass/plots.py
@@ -501,9 +501,9 @@ def densout_pout_from_samples(Mhyd, model, rin_m, rout_m):
 
     nvalm = len(rin_m)
 
-    if Mhyd.cf_prof is not None:
+    rref_m = (rin_m + rout_m) / 2.
 
-        rref_m = (rin_m + rout_m) / 2.
+    if Mhyd.cf_prof is not None:
 
         rad = Mhyd.sbprof.bins
 
@@ -514,8 +514,6 @@ def densout_pout_from_samples(Mhyd, model, rin_m, rout_m):
     else:
 
         cf_prof = Mhyd.ccf
-
-    rref_m = (rin_m + rout_m) / 2.
 
     if Mhyd.fit_bkg:
 
@@ -1101,11 +1099,17 @@ def prof_hires(Mhyd, model, rin=None, npt=200, Z=0.3):
 
         pnt_all = p3d - pth
 
+        sigma_all = np.sqrt(pnt_all * kev2erg * 3. / Mhyd.mup / cgsamu / dens_m) / 1e5 # km/s
+
         mpnt, mpntl, mpnth = np.percentile(pnt_all, [50., 50. - 68.3 / 2., 50. + 68.3 / 2.], axis=1)
+
+        msigma, sigmal, sigmah = np.percentile(sigma_all, [50., 50. - 68.3 / 2., 50. + 68.3 / 2.], axis=1)
 
     else:
 
         mpnt, mpntl, mpnth = np.zeros(len(mptot)), np.zeros(len(mptot)), np.zeros(len(mptot))
+
+        msigma, sigmal, sigmah = np.zeros(len(mptot)), np.zeros(len(mptot)), np.zeros(len(mptot))
 
     dict = {
         "R_IN": rin_m,
@@ -1135,6 +1139,9 @@ def prof_hires(Mhyd, model, rin=None, npt=200, Z=0.3):
         "P_NT": mpnt,
         "P_NT_LO": mpntl,
         "P_NT_HI": mpnth,
+        "SIGMA": msigma,
+        "SIGMA_LO": sigmal,
+        "SIGMA_HI": sigmah,
         "T_COOL": mtc,
         "T_COOL_LO": mtcl,
         "T_COOL_HI": mtch,