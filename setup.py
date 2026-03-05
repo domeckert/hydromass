2e36b5478ed1886b5d4d1c72973303f0c0603e8d
domeckert <Dominique.Eckert@unige.ch>
2026-03-05 14:06:48 +0100

    incrementing version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
       name='hydromass',    # This is the name of your PyPI-package.
-      version='0.7.0',
+      version='0.7.1',
       description='Hydrostatic mass calculator',
       author='Dominique Eckert, Vittorio Ghirardini, Stefano Ettori',
       author_email='Dominique.Eckert@unige.ch',
